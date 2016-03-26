f5e48670f4c9aa215b4d44052e51f044c0e07805
Mark Ng <mng@liquidweb.com>
2016-03-26 06:57:48 -0400

    First!

---
+++ lean/tests/first_tests.py
@@ -0,0 +1,8 @@
+# coding: spec
+from expects import expect, be
+from noseOfYeti.tokeniser.support import noy_sup_setUp
+from unittest import TestCase
+import nose
+
+it 'exists':
+    expect(False).to(be(True))
